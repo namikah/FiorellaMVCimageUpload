1d06ac7d249533f1f6e953f43166bde51dda1212
Namik Heydarov <heydarovnamiq@gmail.com>
2022-02-21 23:49:30 +0400

    CK EDITOR ADDED

--- FirstFiorellaMVC/Models/Blog.cs
+++ FirstFiorellaMVC/Models/Blog.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 
@@ -17,6 +18,8 @@ namespace FirstFiorellaMVC.Models
         [Required(ErrorMessage = "Image Cannot be empty"), MaxLength(50)]
         public string Image { get; set; }
 
+        //[Required(ErrorMessage = "Please upload image.")]
+        //public IFormFile Photo { get; set; }
 
         public DateTime Datetime { get; set; }
 
